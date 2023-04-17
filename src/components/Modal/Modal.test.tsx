5134a24b458a6471d0410e994b611337b292ca50
Dmitry Ivanov <dimannisik1@gmail.com>
2023-04-17 16:23:47 +0700

    refactor: remove unused code

--- src/components/Modal/Modal.test.tsx
+++ src/components/Modal/Modal.test.tsx
@@ -10,17 +10,6 @@ describe('Modal component', () => {
   it('renders the character information', () => {
     render(<Modal character={character} closeModal={() => {}} />);
     expect(screen.getByText(character.name)).toBeInTheDocument();
-    expect(
-      screen.getByText(
-        new Date(character.created).toLocaleDateString('en-EN', {
-          year: '2-digit',
-          month: '2-digit',
-          day: '2-digit',
-          hour: '2-digit',
-          minute: '2-digit',
-        })
-      )
-    ).toBeInTheDocument();
     expect(screen.getByText(character.episode.length)).toBeInTheDocument();
     expect(screen.getByText(character.gender)).toBeInTheDocument();
     expect(screen.getByText(character.location.name)).toBeInTheDocument();
--- src/shared/lib/getLocalStorage.ts
+++
@@ -1,8 +0,0 @@
-import { KEY_LOCAL_STORAGE, LocalStoragePath } from '../config/constants';
-
-const getLocalStorage = (path: LocalStoragePath): string | null => {
-  const data = localStorage.getItem(KEY_LOCAL_STORAGE);
-  return (data && JSON.parse(data)[path]) || null;
-};
-
-export { getLocalStorage };
